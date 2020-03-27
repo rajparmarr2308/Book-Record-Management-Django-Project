b2e96cd5b52c852059086bd6efec51ef5f790d7b
rajparmarr2308 <rajparmar23801@gmail.com>
2020-03-27 11:57:46 +0530

    commit done

---
+++ BRMapp/urls.py
@@ -0,0 +1,14 @@
+from BRMapp import views
+from django.conf.urls import url
+urlpatterns=[
+url('view-books',views.viewBooks),
+url('edit-book',views.editBook),
+url('delete-book',views.deleteBook),
+url('search-book',views.searchBook),
+url('new-book',views.newBook),
+url(r'^add',views.add),
+url('search',views.search),
+url('edit',views.edit),
+url('login',views.userLogin),
+url('logout',views.userLogout),
+]
---
+++ project1/urls.py
@@ -0,0 +1,30 @@
+"""project1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+from django.urls import include
+
+
+urlpatterns = [
+    url('BRMapp/',include('BRMapp.urls')),
+    # path('test/',examapp_view.showtest),
+    # path('result/',examapp_view.showresult),
+    # path('about/',testapp_view.about),
+    # path('contact/',testapp_view.contact),
+    # url('^$',testapp_view.greeting),
+    path('admin/', admin.site.urls),
+]
